b08038188e5aedd4ec0304616ece38965d2be5ad
andrersp <rsp.assistencia@gmail.com>
2018-01-24 03:23:59 -0200

    Secao Pedidos

--- pedidos.py
+++ pedidos.py
@@ -3,6 +3,8 @@
 from PyQt4 import QtGui, QtCore
 from Views.pedidos import Ui_ct_main_pedido
 from Views.confirmacao import Ui_Dialog
+from Views.busca_cliente import Ui_DialogBuscarCliente
+from Class.Funcoes import Funcoes
 
 
 class MainPedidos(Ui_ct_main_pedido):
@@ -10,8 +12,11 @@ class MainPedidos(Ui_ct_main_pedido):
         super(MainPedidos, self).setPedidos(frame)
         self.frame_geral_pedido.show()
 
-        # parametros Inicias
-        # Estilo dos Botoes e Background
+        # Classes
+        self.funcoes = Funcoes()
+
+        # Estilo dos Botoes
+        self.funcoes.estilo_botao_acao(self.bt_cadastrar_pedido, self.bt_cancelar_pedido)
         # Localizar CLiente
         self.bt_pedido_localizar_cliente.setIcon(QtGui.QIcon('Images/buscar.png'))
         self.bt_pedido_localizar_cliente.setIconSize(QtCore.QSize(20, 20))
@@ -19,24 +24,55 @@ class MainPedidos(Ui_ct_main_pedido):
         self.bt_pedido_add_produto.setIcon(QtGui.QIcon('Images/add.png'))
         self.bt_pedido_add_produto.setIconSize(QtCore.QSize(20, 20))
         # Cadastrar Pedido
-        self.bt_cadastrar_pedido.setStyleSheet("color: #FFF; border: 2px solid #0CA3D2; border-radius:10px;"
-                                               "background: #FFF; color: #000")
         self.bt_cadastrar_pedido.clicked.connect(self.confirmar_pedido)
-
         # Cancelar_pedido
         self.bt_cancelar_pedido.setText("Cancelar")
-        self.bt_cancelar_pedido.setStyleSheet("color: #FFF; border: 2px solid red; border-radius:10px;"
-                                              "background: #FFF; color: #000")
 
-        self.tabela_pedido_add.setVerticalHeaderLabels(QtCore.QStringList(["Nome", "Idade"]))
+        # Estilo, acao e comando do Formulário
+        # Buscar Cliente
+        self.bt_pedido_localizar_cliente.clicked.connect(self.janela_busca_cliente)
 
-        # Tamanho, acao  e conteudo da tabela produtos
-        self.tabela_pedidos.setColumnWidth(0, 140)
-        self.tabela_pedidos.setColumnWidth(1, 65)
-        self.tabela_pedidos.setColumnWidth(2, 75)
+
+
+        # Tamanho, acao  e conteudo tabela  produtos pedido
+        self.tabela_pedido_add.setColumnWidth(0, 200)
+        self.tabela_pedido_add.setColumnWidth(1, 70)
+        self.tabela_pedido_add.setColumnWidth(2, 70)
+        self.tabela_pedido_add.setColumnWidth(3, 70)
+        self.tabela_pedido_add.setColumnWidth(4, 30)
+
+        # Tamanho, acao  e conteudo tabela Pedidos Pendentes
+        self.tabela_pedidos.setColumnWidth(0, 40)
+        self.tabela_pedidos.setColumnWidth(1, 80)
+        self.tabela_pedidos.setColumnWidth(2, 160)
         self.tabela_pedidos.horizontalHeader().setResizeMode(QtGui.QHeaderView.Fixed)
         self.tabela_pedidos.verticalHeader().setVisible(False)
 
+    #Janela Buscar cliente
+    def janela_busca_cliente(self):
+        Dialog = QtGui.QDialog()
+        Janela = Ui_DialogBuscarCliente()
+        Janela.setBuscarCliente(Dialog)
+        #Funcoes da Janela
+        def selecionar_cliente_popup(row):
+            self.tx_pedido_cod_cliente.setText(str(Janela.tableWidget.item(row, 0).text()))
+            self.tx_pedido_nome_cliente.setText(Janela.tableWidget.item(row, 1).text())
+            Dialog.close()
+        # Ajustes de Parametros
+
+        #Tabela Resultado
+        Janela.tableWidget.setColumnWidth(0, 50)
+        Janela.tableWidget.setColumnWidth(1, 280)
+        Janela.tableWidget.setItem
+        Janela.tableWidget.horizontalHeader().setVisible(False)
+        Janela.tableWidget.insertRow(0)
+        Janela.tableWidget.setItem(0, 0, QtGui.QTableWidgetItem("50"))
+        Janela.tableWidget.setItem(0, 1, QtGui.QTableWidgetItem("Teste2"))
+        Janela.tableWidget.cellDoubleClicked.connect(selecionar_cliente_popup)
+        Dialog.exec_()
+
+
+    #Janela Confirmação
     def confirmar_pedido(self):
         Janela = Ui_Dialog()
         Dialog = QtGui.QDialog()
